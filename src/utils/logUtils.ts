d510a467abfb62258674c751e7f4485931008fdf
NatsuDrag9 <rohitimandi9@zoho.com>
2025-04-28 21:01:04 +0530

    feat(utils): add log and env utils

---
+++ src/utils/logUtils.ts
@@ -0,0 +1,37 @@
+import { isDevMode } from './envUtils';
+
+// Define a type for the log function parameters to allow any type of arguments
+type LogArgs = unknown[];
+
+// Helper function for logging in Dev mode
+export const logInDev = (...args: LogArgs): void => {
+  if (isDevMode()) {
+    if (typeof window !== 'undefined') {
+      // Client-side
+      console.log(
+        '%c∴ LOGGING IN DEV',
+        'font-family: monospace; font-size: 12px; color: green;'
+      );
+      console.log(...args);
+    } else {
+      // Server-side
+      console.log('∴ LOGGING IN DEV:', ...args);
+    }
+  }
+};
+
+export const logErrorInDev = (...args: LogArgs): void => {
+  if (isDevMode()) {
+    if (typeof window !== 'undefined') {
+      // Client-side
+      console.log(
+        '%c∴ LOGGING ERROR IN DEV',
+        'font-family: monospace; font-size: 12px; color: red;'
+      );
+      console.error(...args);
+    } else {
+      // Server-side
+      console.error('∴ LOGGING ERROR IN DEV:', ...args);
+    }
+  }
+};
