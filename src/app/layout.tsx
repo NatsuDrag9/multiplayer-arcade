6584f6d0d5a2c3eaf9916148883112b84c2808c5
NatsuDrag9 <rohitimandi9@zoho.com>
2025-05-04 21:43:04 +0530

    fix(app): create app-provider to provide store to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/styles/main.scss';
+import { AppProviders } from './Providers';
 // import './globals.css';
 
 const geistSans = Geist({
@@ -34,7 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppProviders>{children}</AppProviders>
       </body>
     </html>
   );
